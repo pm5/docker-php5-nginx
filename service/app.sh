6b71ecb443866d64bad485ca17135c057c55155c
Pomin Wu <pomin5@gmail.com>
2015-01-28 17:41:53 +0800

    fix app service script
    create homedir if /home is set to volume

--- service/app.sh
+++ service/app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 if [ ! -f /home/app/password.txt ]; then
+  mkdir -p /home/app
+  chown -R app:www-data /home/app
+  chmod 755 /home/app
+  mkdir -p /var/www
   chown -R app:www-data /var/www
   chmod 755 /var/www
   ln -s /var/www /home/app/public
